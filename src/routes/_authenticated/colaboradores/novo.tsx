a5ed76f877ba40c26ccc6e66435eb2eddcffce4a
Renato <renato.yw81@gmail.com>
2026-06-15 20:00:36 -0300

    Delete src directory

--- src/routes/_authenticated/colaboradores/novo.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { EmployeeForm } from "@/components/employees/employee-form";
-
-export const Route = createFileRoute("/_authenticated/colaboradores/novo")({
-  head: () => ({ meta: [{ title: "Novo colaborador · SIT" }] }),
-  component: () => <EmployeeForm />,
-});
